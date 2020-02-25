3dc2609f09355903f1e5c56549595c7469eb8d96
Matteo <matteop18@gmail.com>
2020-02-25 14:35:43 +0100

    primo commit

---
+++ server.php
@@ -0,0 +1,53 @@
+<?php
+$database = [
+  [
+    'title' => 'New Jersey',
+    'author' => 'Bon Jovi',
+    'year' => 1988,
+    'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+  ],
+  [
+    'title' => 'Faccio un casino',
+    'author' => 'Coez',
+    'year' => 2017,
+    'poster' => 'https://img.discogs.com/9G4_chDMrst5JhWAeK3Ak2kLMtg=/fit-in/600x537/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10269901-1494429619-1062.jpeg.jpg'
+  ],
+  [
+    'title' => 'TheManInTheRadiator',
+    'author' => 'Bones',
+    'year' => 1988,
+    'poster' => 'https://img.discogs.com/hhp6xTYMfMnhUlgSOE0y-qhhtEo=/fit-in/554x554/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-12760733-1541431232-4362.jpeg.jpg'
+  ],
+  [
+    'title' => 'Star Shopping',
+    'author' => 'Lil Peep',
+    'year' => 2015,
+    'poster' => 'https://img.discogs.com/yhLs5XIGtrqRkaDwwdyOYvrsClo=/fit-in/500x500/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-11376176-1515202351-1738.jpeg.jpg'
+  ],
+  [
+    'title' => 'Sunset Lover',
+    'author' => 'Petit Biscuit',
+    'year' => 2016,
+    'poster' => 'https://img.discogs.com/khrhWxGOPj-h75rciFQczcK7Q8E=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-8991367-1472892694-6129.jpeg.jpg'
+  ],
+  [
+    'title' => 'Greatest Hits',
+    'author' => 'Bruce Springsteen',
+    'year' => 1995,
+    'poster' => 'https://img.discogs.com/Y0Job_J0jeLUi5F-hVJwuyy_DUI=/fit-in/320x320/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2319771-1337362484-1036.jpeg.jpg'
+  ],
+  [
+    'title' => 'Faded',
+    'author' => 'Alan Walker',
+    'year' => 2015,
+    'poster' => 'https://img.discogs.com/BYJANHK_B8M3Q6qVPRbQXtucowc=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-8048188-1454155027-4231.jpeg.jpg'
+  ],
+  [
+    'title' => 'Moonlight Shadow',
+    'author' => 'Mike Oldfield',
+    'year' => 1983,
+    'poster' => 'https://img.discogs.com/imTvzEgUVdqotIBMEgIgcK6r1IA=/fit-in/600x609/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-259136-1482871858-5312.jpeg.jpg'
+  ]
+];
+var_dump($database)
+?>
