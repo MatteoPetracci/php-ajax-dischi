7c06cc1396e18a2ffface93c2ee2375b9770765a
Matteo <matteop18@gmail.com>
2020-02-26 11:56:58 +0100

    installato jquery e handlebars

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.sass('src/app.scss', 'dist/').options({
+mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/').options({
       processCssUrls: false
    });;
 
